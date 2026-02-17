7f40ac2f086e2c7a70831b84347ae7909a2abb20
TexasStreamTeam <TxStreamTeam@txstate.edu>
2026-02-17 16:04:22 -0600

    Update Water_Validation_App_Revised_Feb13.py

--- Water_Validation_App_Revised_Feb13.py
+++ Water_Validation_App_Revised_Feb13.py
@@ -668,7 +668,7 @@ df = df.drop(columns=["_site_norm"], errors="ignore")
 exclusion_report = pd.DataFrame(exclusion_records)
 wide_counts = build_site_param_count_table(df, category_cols)
 
-return df.reset_index(drop=True), exclusion_report, wide_counts
+    return df.reset_index(drop=True), exclusion_report, wide_counts
     
  
 
