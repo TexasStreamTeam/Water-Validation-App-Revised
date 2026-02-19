764f62565a590e5d82c640c1ccaf65b29f857d0c
TexasStreamTeam <TxStreamTeam@txstate.edu>
2026-02-19 13:27:38 -0600

    Update Water_Validation_App_Revised_Feb13.py

--- Water_Validation_App_Revised_Feb13.py
+++ Water_Validation_App_Revised_Feb13.py
@@ -645,7 +645,7 @@ df_param_filtered = apply_param_level_exclusions(
     # ---------------------------------------------------
     # Watershed rule: >= 3 sites
     # ---------------------------------------------------
-    if watershed_col:
+if watershed_col:
         ws_counts = (
             df_param_filtered.groupby(watershed_col)[site_col]
             .nunique()
