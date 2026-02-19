3dec04c2bd6f04add4585805b053be335cfb78df
TexasStreamTeam <TxStreamTeam@txstate.edu>
2026-02-19 14:38:32 -0600

    Update Water_Validation_App_Revised_Feb13.py

--- Water_Validation_App_Revised_Feb13.py
+++ Water_Validation_App_Revised_Feb13.py
@@ -864,7 +864,7 @@ with tabs[7]:
                 key="download_exclusion"
             )
 
-        if wide_counts is not None and not wide_count_table.empty:
+        if wide_count_table is not None and not wide_count_table.empty:
             buf_wide = io.BytesIO()
             wide_count_table.to_csv(buf_wide, index=False)
             st.download_button(
